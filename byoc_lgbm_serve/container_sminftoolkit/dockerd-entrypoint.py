c77ee69dddae873b6541950b7d626496fb64ce07
yito0427 <y.ito.0427@gmail.com>
2022-10-24 05:00:53 +0000

    fix notebook

--- byoc_lgbm_serve/container_sminftoolkit/dockerd-entrypoint.py
+++ byoc_lgbm_serve/container_sminftoolkit/dockerd-entrypoint.py
@@ -1,3 +1,4 @@
 from sagemaker_inference import model_server
-print('========== This is dockerd-entrypoint.py =============')
+print('========== This is START of dockerd-entrypoint.py =============')
 model_server.start_model_server()
+print('========== This is END of dockerd-entrypoint.py =============')
