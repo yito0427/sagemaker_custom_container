a1ab6ebdea09d3150979bc7bb6ff94b9f30d3222
yito0427 <y.ito.0427@gmail.com>
2022-11-09 03:00:00 +0000

    update

--- byoc_lgbm_serve/src_builtin_container_serve/inference.py
+++ byoc_lgbm_serve/src_builtin_container_serve/inference.py
@@ -1,97 +1,70 @@
 #!/usr/bin/env python
 
-#from __future__ import print_function
 import lightgbm as lgb
-#import argparse
 import os
-#from os import path
-#import logging
-#import pickle
-#import json
-#import pandas as pd
-#import glob
 
 # 推論用エンドポイントを作成する際に用いるモデルの読み込み用処理
 def model_fn(model_dir):
-    print('=================================')
-    print('=================================')
-    print('==========my model_fn==============')
-    print('=================================')
-    print('=================================')
-    
+    print('============== START: model_fn =================')
+    print('='*50)
     os.system('pwd')
+    print('='*50)
     os.system('ls')
-    print('==============a===================')
+    print('='*50)
     os.system('ls /opt/ml/model')
-    print('===============a==================')
-    
-    
+    print('='*50)
+
     print('==================================')
     print('======= Display ENV values =======')
     print('==================================')    
-
     for key in os.environ:
         val = os.environ[key]
         print('{}: {}'.format(key, val))
+        
     print('=================================')
     print('=======START LOAD MODEL===========')
     print('=================================')
-    #with open(path.join(model_dir, 'model.pickle'), 'rb') as f:
-    #    model = pickle.load(f)
     model = lgb.Booster(model_file='/opt/ml/model/lightgbm-regression-model.txt')
-    print('=================================')
-    print('=======END OF model_fn===========')
-    print('=================================')
+    
+    print('============== END: model_fn =================')
     return model
 
 def predict_fn(transformed_data, model):
-    print('=================================')
-    print('=================================')
-    print('==========my predict_fn =========')
-    print('=================================')
-    print('=================================')
+    print('============== START: predict_fn =================')
     
-    print('=================================')
-    print('=======transformed_data===========')
-    print('=================================')
+    print('====================================')
+    print('======= transformed_data ===========')
+    print('====================================')
     print(type(transformed_data))
     print(transformed_data)
-    print('=================================')
     
+    print('========= predict ============')
     pred = model.predict(transformed_data)
+    
     print('===============================================')
     print('============== result of pred =================')
     print('===============================================')
     print(type(pred))
     print(pred)
     
-    print('============== END: result of pred =================')
-    
+    print('============== END: predict_fn =================')
     return pred
 
 def input_fn(input_data, content_type):
-    print('=================================')
-    print('=================================')
-    print('==========my input_fn =========')
-    print('=================================')
-    print('=================================')
-    ### リストは二次元配列にする必要がある。
-    ### まず、\nで区切る
-    ### , で区切る
-    print(content_type)
-    print('========== input_data =========')
+    print('============== START: input_fn =================')
+    
+    print('====================================')
+    print('======= Check input_data ===========')
+    print('====================================')
+    print(f'content_type={content_type}')
     print(type(input_data))
     print(input_data)
-    print('========== END: input_data =========')
+    
+    ### リストは二次元配列にする必要がある。まず、'\n'で区切る。その後','で区切る
     if content_type == 'text/csv':
-        transformed_data = input_data.splitlines()
-        #transformed_data = input_data.split(',')
-        print(type(transformed_data))
-        print(transformed_data)
-        print(len(transformed_data)) # 102行
-        print('========== END: transformed_data =========')
         transformed_data = [s.split(',') for s in input_data.splitlines()]
-        
     else:
         raise ValueError("Illegal content type")
+
+    print('============== END: input_fn =================')
     return transformed_data
